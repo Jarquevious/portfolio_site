ab5b02230cc87248e94854d3f23b393a7dbad9f0
Jarquevious <jarquevious@gmail.com>
2020-04-06 20:49:08 -0700

    changes made

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="home"),
-    path('contact/', views.contact, name="contact"),
+    path('', views.home),
+    path('contact/', views.contact),
     path('greet/<str:name>/', views.greet_by_name),
 ]
